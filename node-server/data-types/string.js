d1f6d7c75128e89bf281d7fdd42af9a5768f4f19
EXT Student <extstudent@AD.UCSD.EDU>
2019-10-05 11:54:46 -0700

    week 2 finished

--- node-server/data-types/string.js
+++ node-server/data-types/string.js
@@ -1,49 +1,69 @@
-var str = 'hello world';
-var hello = 0
+// var str = 'hello world';
+// var hello = 0
 
-var arr = [str, hello, "two", 3, function () { console.log("yes") }]
+// var arr = [str, hello, "two", 3, function () { console.log("yes") }]
 
-var obj = { "one": str, 2: hello, func: function () { console.log("yes") } }
+// var obj = { "one": str, 2: hello, func: function () { console.log("yes") } }
 
-console.log(str)
+// console.log(str)
 
-console.log(typeof str)
+// console.log(typeof str)
 
-console.log(hello)
-
-
-console.log(typeof hello)
+// console.log(hello)
 
 
-console.log('my array hello :' + arr[1])
-
-console.log('my object  :' + obj)
-
-console.log('my object  :' + obj.one)
-
-console.log(arr[4])
-
-console.log('my object  :' + obj.func)
+// console.log(typeof hello)
 
 
-let num = 200
+// console.log('my array hello :' + arr[1])
 
-const loop = (num) => {
+// console.log('my object  :' + obj)
 
-    for (let i = 0; i <= num; i++) {
-        console.log(`This is my count ${i}`)
+// console.log('my object  :' + obj.one)
+
+// console.log(arr[4])
+
+// console.log('my object  :' + obj.func)
+
+
+let whichLoop = (str, num) => {
+    console.log(str, num)
+
+    if (str === "loop") {
+
+        const loop = (num) => {
+
+            for (let i = 0; i <= num; i++) {
+                console.log(`This is my count ${i}`)
+            }
+        }
+        loop(num)
+
+    } else if (str === "while") {
+
+        const loopWhile = (num) => {
+            count = 0
+            while (count < num) {
+                count++
+                console.log(count)
+            }
+            console.log(`This is my while count ${count}`)
+        }
+
+        loopWhile(num)
     }
+
 }
 
-const loopWhile = (num) => {
-    count = 0 
-    while(count < num){
-        count++
-        console.log(count)
-    }
-    console.log(`This is my while count ${count}`)
-}
 
-// loop(num)
 
-loopWhile(num)
+
+
+whichLoop("loop", 10 )
+
+whichLoop('while', 1)
+
+var str1 = "racecar"
+var str2 = "racecar"
+console.log(str1 === str2)
+console.log('racecar'.split("").join(""))
