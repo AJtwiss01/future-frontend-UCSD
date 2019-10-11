16e4a79b77c7dba177c9cdf9b1f77880a575c51e
Arthur Twiss <twissaj01@gmail.com>
2019-10-10 17:32:03 -0700

    for tonight

--- ffs-week2/node-server/index.js
+++ ffs-week2/node-server/index.js
@@ -3,15 +3,20 @@ const server = express();
 const path = require('path');
 const router = express.Router();
 
-router.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/public/index.html'));
-  //__dirname : It will resolve to your project folder.
-});
+// router.get('/',function(req,res){
+//   res.sendFile(path.join(__dirname+'/public/index.html'));
+//   //__dirname : It will resolve to your project folder.
+// });
 
 
 //add the router
+server.set('view engine', 'pug');
 server.use('/', router);
 server.use(express.static(__dirname +'/public'))
+
+server.get('/', function (req, res) {
+  res.render('index', { title: 'Hey', message: 'Hello there!' })
+})
 server.post('/', function(req, res){
   console.log(req);
   res.send("recieved your request!");
